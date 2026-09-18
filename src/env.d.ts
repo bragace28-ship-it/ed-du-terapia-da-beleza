194c7e845d897a2f4da037e9faa290c731d3cfef
bragace28-ship-it <bragace28@gmail.com>
2026-09-18 11:52:15 -0300

    chore: remove legacy application layer before clean block implementation

--- src/env.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference types="vite/client" />
-interface ImportMetaEnv {
-  readonly VITE_APP_VERSION?: string
-  readonly VITE_APP_COMMIT?: string
-}
-interface ImportMeta { readonly env: ImportMetaEnv }
