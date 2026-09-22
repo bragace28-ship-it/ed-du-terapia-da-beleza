72a26b2789095185f731ec6e083b3b9484acee94
bragace28-ship-it <bragace28@gmail.com>
2026-09-22 15:21:13 -0300

    build(block09-17): include platform core runtime

--- runtime/v33-entry.js
+++ runtime/v33-entry.js
@@ -2,3 +2,4 @@ import './v33-auth-pin.js';
 import './v33-block03-comandas.js';
 import './v33-block04-gateways.js';
 import './v33-block06-08-agenda-financeiro.js';
+import './v33-block09-17-platform-core.js';
